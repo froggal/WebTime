54474fd840c9d0e81710924a6907af360f60e8cf
froggal <doeon111@naver.com>
2022-07-23 14:19:25 +0900

    Zerofill

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 function displayTime(){
     let time = new Date();
-    let hours = time.getHours();
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
+    let hours = String(time.getHours()).padStart(2, '0');
+    let minutes = String(time.getMinutes()).padStart(2, '0');
+    let seconds = String(time.getSeconds()).padStart(2, '0');
+
     document.getElementById('NowTime').innerHTML = hours+':'+minutes+':'+seconds;
 }
-
+displayTime();
 setInterval(displayTime, 1000);

7911fcba9f18ad5525df7d70602bd8c916ac3b9a
froggal <doeon111@naver.com>
2022-07-23 14:25:26 +0900

    Clean up

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ function displayTime(){
     let hours = String(time.getHours()).padStart(2, '0');
     let minutes = String(time.getMinutes()).padStart(2, '0');
     let seconds = String(time.getSeconds()).padStart(2, '0');
-
     document.getElementById('NowTime').innerHTML = hours+':'+minutes+':'+seconds;
 }
+
 displayTime();
 setInterval(displayTime, 1000);

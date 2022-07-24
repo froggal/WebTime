c661e6a5525d5a89789c0b291cce7aff5bdee61a
froggal <doeon111@naver.com>
2022-07-24 15:36:19 +0900

    restyling & insert javascript code.

--- src/app.js
+++ src/app.js
@@ -8,3 +8,41 @@ function displayTime(){
 
 displayTime();
 setInterval(displayTime, 1000);
+
+// Script by https://stickode.tistory.com/230
+
+let timerId;
+let time = 0;
+const stopwatch = document.getElementById("stopwatch");
+let  hour, min, sec;
+
+function printTime() {
+    time++;
+    stopwatch.innerText = getTimeFormatString();
+}
+
+function startClock() {
+    printTime();
+    stopClock();
+    timerId = setTimeout(startClock, 1000);
+}
+
+function stopClock() {
+    if (timerId != null) {
+        clearTimeout(timerId);
+    }
+}
+
+function resetClock() {
+    stopClock()
+    stopwatch.innerText = "00:00:00";
+    time = 0;
+}
+
+function getTimeFormatString() {
+    hour = parseInt(String(time / (60 * 60)));
+    min = parseInt(String((time - (hour * 60 * 60)) / 60));
+    sec = time % 60;
+
+    return String(hour).padStart(2, '0') + ":" + String(min).padStart(2, '0') + ":" + String(sec).padStart(2, '0');
+}
